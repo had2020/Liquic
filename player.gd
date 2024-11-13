1cc84272deb1cf3b1b857138e4913c31e4aca3ac
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-13 13:39:59 -0800

    Update player.gd

--- player.gd
+++ player.gd
@@ -1,17 +1,17 @@
 extends RigidBody2D
 
-var move_speed = Vector2(100,0)
-
 var last_mouse_pos = Vector2(0,0)
 
 # dirctional-conditionals
 var directional_change = false
 var directional = 0 # 0 null, 1, foward, 2 back, 3 left, 4 right, 5 left stafe, 6 right stafe
 
+# 1, foward, 2 back, 3 left, 4 right
+var thrusts_array = [1.0, 1.0, 1.0, 1.0]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,18 +25,18 @@ func _process(delta: float) -> void:
 		match directional:
 			0: # null
 				pass
-			1: # foward 
-				self.apply_impulse(move_speed)
+			1: # foward                    #TODO 0 changes direction
+				self.apply_impulse(Vector2(0,-thrusts_array[0]))
 			2: # back
-				self.apply_impulse(move_speed)
+				self.apply_impulse(Vector2(0,thrusts_array[1]))
 			3: # left
-				self.apply_impulse(move_speed)
+				self.apply_impulse(Vector2(-thrusts_array[2],0))
 			4: # right
-				self.apply_impulse(move_speed)
+				self.apply_impulse(Vector2(thrusts_array[3],0))
 			5: # stafe left
-				self.apply_impulse(move_speed)
+				self.apply_impulse(Vector2(thrusts_array[2]/2,thrusts_array[2]/2))
 			6: # stafe right
-				self.apply_impulse(move_speed)
+				self.apply_impulse(Vector2(thrusts_array[3]/2,thrusts_array[3]/2))
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Foward"):
