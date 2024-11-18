a207bb3ae48c8f726750b20369c90d95323a5a21
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-17 19:07:01 -0800

    basic build mode controls working
    used better method for moving camera in build mode, and created methods for adding build level. Also added build mode and build cursor. hide old cursor as well when in build mode.

--- level.gd
+++ level.gd
@@ -11,37 +11,51 @@ func _process(delta: float) -> void:
 	pass
 
 func freeze_node(node):
-	#node.visiblity = false  TODO change trans or visablity
-	node.set_process(false)
-	if node.get("sleeping") != null:
-		node.sleeping = true
-	node.set_physics_process(false)
-	if node.get_child_count() != 0:
-		var active_objects = node.get_children()
-		for child in active_objects:
-			freeze_node(child)
+	#node.visiblity = false  TODO change trans or visablity or ordering
+	if not node.is_in_group("non-build-freeze"):
+		if node.is_in_group("hide_when_frooze"):
+			node.visible = false
+		node.set_process(false)
+		if node.get("sleeping") != null:
+			node.sleeping = true
+		node.set_physics_process(false)
+		if node.get_child_count() != 0:
+			var active_objects = node.get_children()
+			for child in active_objects:
+				freeze_node(child)
 
 func unfreeze_node(node):
-	node.set_process(true)
-	if node.get("sleeping") != null:
-		node.sleeping = false
-	node.set_physics_process(true)
-	if node.get_child_count() != 0:
-		var active_objects = node.get_children()
-		for child in active_objects:
-			unfreeze_node(child)
+	if not node.is_in_group("non-build-freeze"):
+		if node.is_in_group("hide_when_frooze"):
+			node.visible = true
+		node.set_process(true)
+		if node.get("sleeping") != null:
+			node.sleeping = false
+		node.set_physics_process(true)
+		if node.get_child_count() != 0:
+			var active_objects = node.get_children()
+			for child in active_objects:
+				unfreeze_node(child)
+
+func spawn_build_level():
+	pass
+
+func despawn_build_level():
+	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Build Mode"):
 		var active_objects = $Active.get_children()
 		if Globals.build_mode == false:
 			Globals.build_mode = true
+			spawn_build_level()
 			#print("Player entered Build Mode.")
 			for child in active_objects:
 				freeze_node(child)
 		else:
 			#print("Player exited Build Mode.")
 			Globals.build_mode = false
+			despawn_build_level()
 			for child in active_objects:
 				unfreeze_node(child)
 	
